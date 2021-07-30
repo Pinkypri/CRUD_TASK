0595284331b855e8a621d2b67d0896cac4b44260
Muthupriya P <65939169+Pinkypri@users.noreply.github.com>
2021-07-30 00:14:17 -0700

    Add files via upload

--- routes/api/product/index.js
+++ routes/api/product/index.js
@@ -1,6 +1,7 @@
 const  router=require("express").Router();
 
 
+const { db } = require("../../../models/Product");
 const Product =require("../../../models/Product");
 
  
@@ -26,7 +27,7 @@ router.put("/update/:id",async(req,res)=>{
           { _id:req.params.id},
            {
                id:req.body.id,
-               sku:req.body.sku,
+               SKU:req.body.SKU,
                category:req.body.category,
                name:req.body.name,
                description:req.body.description,
@@ -54,10 +55,28 @@ router.put("/update/:id",async(req,res)=>{
       }
   })
 
-
-router.get("/get/sort",async(req,res)=>{
+  router.get("/name",async(req,res)=>{
     try {
-        const data=await Product.find({}).sort({price:1});
+        const user=await Product.find({name:{"$regex":"f"}});
+       return res.json(user)
+    } catch (error) {
+       return res.json ({msg:error.message});
+    }
+})
+
+router.get("/filter",async(req,res)=>{
+    try {
+        const user=await Product.find({},{name:1,_id:0,price:1,SKU:1});
+       return res.json(user)
+    } catch (error) {
+       return res.json ({msg:error.message});
+    }
+})
+
+
+router.get("/sort",async(req,res)=>{
+    try {        
+        const data=await Product.find().sort({price:1});
         return res.json(data);
     } catch (error) {
         return res.json({msg:error.message})
@@ -65,16 +84,29 @@ router.get("/get/sort",async(req,res)=>{
     }
 });
 
-// router.get("/all/products",async(req,res)=>{
-//     try {
-//         const user=await Product.find({}).populate.select("-__v -_id  -createdAt -updatedAt");
-//         res.json("");
-//     } catch (error) {
-//          res.json(error); 
-//     }
-//    ;
-// });
-   
+router.get("/aggregate/:category/:price1-:price2",async(req,res)=>{
+    try {        
+        const data=await Product.find(
+        {category:req.params.category,
+            price:{$gte:req.params.price1,$lte:req.params.price2}})
+            .sort({price:1})
+    
+        return res.json(data);
+    } catch (error) {
+        return res.json({msg:error.message});
+        
+    }
+});
 
+   
+router.get("/all/products",async(req,res)=>{
+    try {
+        const user=await Product.find({name:{$regex:"f"}}).populate.select("-__v -_id  -createdAt -updatedAt");
+        res.json("");
+    } catch (error) {
+         res.json(error); 
+    }
+   ;
+});
   
 module.exports=router;
